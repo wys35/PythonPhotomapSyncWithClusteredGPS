86b3fe2392a5f67e82d0ba6835337bc0e854f0bf
Eddy Wu <wuyuansheng@gmail.com>
2018-09-03 08:20:32 +0800

    references added

--- runme.ps1
+++ runme.ps1
@@ -1,4 +1,5 @@
 write-host "install numpy, scikit-learn, copy photos to img directory and then run this script"
+write-host "pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-learn"
 Write-HOst "Googleapis.com access is required"
 python .\getexifdata.py
 python .\dbscanit.py
--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 echo "install numpy, scikit-learn, copy photos to img directory and then run this script"
+echo "pip3 install --user numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-learn"
 echo "Googleapis.com access is required"
 python3 ./getexifdata.py
 python3 ./dbscanit.py
