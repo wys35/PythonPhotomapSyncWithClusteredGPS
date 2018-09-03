e58fdef0e7216bb9ef60bfb3bdd49f166ccf43d3
Eddy Wu <wuyuansheng@gmail.com>
2018-09-03 08:23:05 +0800

    new lines

--- runme.ps1
+++ runme.ps1
@@ -1,6 +1,8 @@
 write-host "install numpy, scikit-learn, copy photos to img directory and then run this script"
 write-host "pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-learn"
 Write-HOst "Googleapis.com access is required"
+write-host "`n`n"
+
 python .\getexifdata.py
 python .\dbscanit.py
 python .\getLocationName.py
--- runme.sh
+++ runme.sh
@@ -1,6 +1,7 @@
-echo "install numpy, scikit-learn, copy photos to img directory and then run this script"
-echo "pip3 install --user numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-learn"
-echo "Googleapis.com access is required"
+printf "install numpy, scikit-learn, copy photos to img directory and then run this script"
+printf "pip3 install --user numpy scipy matplotlib ipython jupyter pandas sympy nose scikit-learn"
+printf "Googleapis.com access is required"
+printf "\n"
 python3 ./getexifdata.py
 python3 ./dbscanit.py
 python3 ./getLocationName.py
